ac947e13c835202b6d86d1fc58a97804232f01a5
whw3 <gogs@whw3.com>
2017-06-18 14:26:14 -0500

    added conditional build feature

--- build.sh
+++ build.sh
@@ -28,20 +28,24 @@ else
     echo "I'm confused"
     exit 2
 fi
+
+# Always remove and refresh
+[[ -d  /srv/docker/golang-official/golang ]] &&  \
+  rm -rf /srv/docker/golang-official/golang
+
 cd /srv/docker/golang-official
-if [[ -d  golang ]]; then
-    rm -rf golang
-fi
 git clone https://github.com/docker-library/golang.git
 patch -p0 < golang.patch
 
 if [ "$ALPINE" = "1" ]; then
-    if [[ ! -d /srv/docker/alpine ]]; then
-        cd /srv/docker/
-        git@git:Docker/alpine.git
+    if [[ "$(docker images -q alpine 2> /dev/null)" == "" ]]; then
+        if [[ ! -d /srv/docker/alpine ]]; then
+            cd /srv/docker/
+            git@git:Docker/alpine.git
+        fi
+        cd /srv/docker/alpine
+        /srv/docker/alpine/build.sh
     fi
-    cd /srv/docker/alpine
-    /srv/docker/alpine/build.sh
     cd /srv/docker/golang-official/golang/1.8/alpine3.6/
 else
     cd /srv/docker/golang-official/golang/1.8/
